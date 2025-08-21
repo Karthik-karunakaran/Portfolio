4f836570aa2884e7a4e07c833a4fee90edeeedc9
Karthik-karunakaran <kk5035236@gmail.com>
2025-08-21 15:21:40 +0530

    change

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ function Footer(){
         <>
              <div className=" footer">
                 <div className="container">
-                    <p><span> <i class="fa-regular fa-copyright copy-right me-1"></i></span> Karthik. All rights reserved
+                    <p><span> <i class="fa-regular fa-copyright copy-right me-1"></i></span> Developed by Karthik 
                     <span><a href="https://www.linkedin.com/in/k-karthik-201540293/" className="icon-btn"> <i class="fa-brands fa-linkedin"></i></a></span>
                     <span><a href="https://github.com/Karthik-karunakaran" className="icon-btn"><i className="fa-brands fa-github"></i></a></span></p>
                 </div>
